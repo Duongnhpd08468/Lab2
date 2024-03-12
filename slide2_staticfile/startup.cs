b4d3f150b8370a29ef9f6edfadf145b1ee38ec01
Duongnhpd08468 <rinseki9@gmail.com>
2024-03-12 20:00:02 +0700

    Lab2

---
+++ slide2_staticfile/startup.cs
@@ -0,0 +1,18 @@
+﻿namespace slide2_staticfile
+{
+    public class startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStaticFiles();
+            }
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello static file");
+            });
+        }
+    }
+}
